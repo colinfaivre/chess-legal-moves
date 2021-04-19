016f79359aaf9e52a3d09acef9b695c409a42a2d
Colin Faivre <colin.faivre@gmail.com>
2021-04-19 17:21:02 +0200

    Expose getLegalMoves() and publish 0.0.9 to npm

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import getLegalMoves from './getLegalMoves';
+export { getLegalMoves };
 
-console.log(getLegalMoves('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'));
+// console.log(getLegalMoves('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'));
