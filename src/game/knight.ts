507a8bdd3ed8a95f53f2c6d51603100568134840
Colin Faivre <colin.faivre@gmail.com>
2021-11-18 18:59:19 +0100

    Get the knight move generation for all moves working

--- src/game/knight.ts
+++ src/game/knight.ts
@@ -1,43 +1,24 @@
-import { IMovesFromPosition } from "../types"
+import { IMovesFromPosition, IPosition } from "../types"
 import BitBoard from "../bitboard/bitboard"
 import { positionsTable } from "../bitboard/positionsHashTable"
 
 export function generateKnightsMoves(knightsBB: BitBoard): IMovesFromPosition[] {
-    // const knightsList = knightsBB.extractBits().map((knightPositionCode) => {
-    //     const moves = getKnightMoves(knightPositionCode)
+    const killMoves: IPosition[] = []
+    const allKnightMovesTable = generateAllKnightMovesBBTable();
 
-    //     return {
-    //         from: positionsTable[knightPositionCode],
-    //         quietMoves: moves.quiet,
-    //         killMoves: moves.kill,
-    //     }
-    // })
+    const knightsList = knightsBB.extractBits().map((knightPositionCode) => {
+        return {
+            from: positionsTable[knightPositionCode],
+            quietMoves: allKnightMovesTable[knightPositionCode].extractBits().map(knightDestination => positionsTable[knightDestination]),
+            killMoves,
+        }
+    })
 
-    return [{
-        from: 'b1',
-        quietMoves: ['a3', 'c3'],
-        killMoves: [],
-    },
-    {
-        from: 'g1',
-        quietMoves: ['f3', 'h3'],
-        killMoves: [],
-    }]
+    return knightsList;
 }
 
 function generateAllKnightMovesBBTable(): BitBoard[] {
     // A list of 64 bitboards representing the knight possible moves from any board position
-    const KNIGHT_DIRECTIONS = {
-        noNoEa: 17,
-        noEaEa: 10,
-        soEaEa: -6,
-        soSoEa: -15,
-        noNoWe: 15,
-        noWeWe: 6,
-        soWeWe: -10,
-        soSoWe: -17,
-    }
-
     const AFile = [0, 8, 16, 24, 32, 40, 48, 56];
     const BFile = [1, 9, 17, 25, 33, 41, 49, 57];
     const ABFile = [...AFile, ...BFile];
@@ -45,32 +26,31 @@ function generateAllKnightMovesBBTable(): BitBoard[] {
     const GFile = [6, 14, 22, 30, 38, 46, 54, 62];
     const GHFile = [...GFile, ...HFile];
 
-    function noNoEa(position: number): BitBoard {
-        if (!AFile.includes(position)) return BitBoard.fromPos(position + KNIGHT_DIRECTIONS.noNoEa);
+    function noNoEa(position: number): number {
+        return !AFile.includes(position + 17) ? position + 17 : null;
     }
-    function noEaEa(position: number): BitBoard {
-        if (!ABFile.includes(position)) return BitBoard.fromPos(position + KNIGHT_DIRECTIONS.noEaEa);
+    function noEaEa(position: number): number {
+        return !ABFile.includes(position + 10) ? position + 10 : null;
     }
-    function soEaEa(position: number): BitBoard {
-        if (!ABFile.includes(position)) return BitBoard.fromPos(position + KNIGHT_DIRECTIONS.soEaEa);
+    function soEaEa(position: number): number {
+        return !ABFile.includes(position - 6) ? position - 6 : null;
     }
-    function soSoEa(position: number): BitBoard {
-        if (!AFile.includes(position)) return BitBoard.fromPos(position + KNIGHT_DIRECTIONS.soSoEa);
+    function soSoEa(position: number): number {
+        return !AFile.includes(position - 15) ? position - 15 : null;
     }
-    function noNoWe(position: number): BitBoard {
-        if (!HFile.includes(position)) return BitBoard.fromPos(position + KNIGHT_DIRECTIONS.noNoWe);
+    function noNoWe(position: number): number {
+        return !HFile.includes(position + 15) ? position + 15 : null;
     }
-    function noWeWe(position: number): BitBoard {
-        if (!GHFile.includes(position)) return BitBoard.fromPos(position + KNIGHT_DIRECTIONS.noWeWe);
+    function noWeWe(position: number): number {
+        return !GHFile.includes(position + 6) ? position + 6 : null;
     }
-    function soWeWe(position: number): BitBoard {
-        if (!GHFile.includes(position)) return BitBoard.fromPos(position + KNIGHT_DIRECTIONS.soWeWe);
+    function soWeWe(position: number): number {
+        return !GHFile.includes(position - 10) ? position - 10 : null;
     }
-    function soSoWe(position: number): BitBoard {
-        if (!HFile.includes(position)) return BitBoard.fromPos(position + KNIGHT_DIRECTIONS.soSoWe);
+    function soSoWe(position: number): number {
+        return !HFile.includes(position - 17) ? position - 17 : null;
     }
     
-    function combilePreviousBB(): BitBoard { return BitBoard.fromPos(0) }
     const allKnightMoves: BitBoard[] = [];
     
     for (let position = 0; position < 63; position++) {
@@ -84,7 +64,7 @@ function generateAllKnightMovesBBTable(): BitBoard[] {
         if (soWeWe(position)) movesFromThisPosition.push(soWeWe(position));
         if (soSoWe(position)) movesFromThisPosition.push(soSoWe(position));
 
-        allKnightMoves.push(combilePreviousBB());
+        allKnightMoves.push(BitBoard.fromPositions(movesFromThisPosition));
     }
 
     return allKnightMoves;
