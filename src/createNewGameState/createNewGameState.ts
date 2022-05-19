502237b2dbd73bf24aab14ee381918c0aa90c028
Colin Faivre <colin.faivre@gmail.com>
2022-05-19 11:31:49 +0200

    test(createNewGameState): :white_check_mark: add edge cases tests

--- src/createNewGameState/createNewGameState.ts
+++ src/createNewGameState/createNewGameState.ts
@@ -10,7 +10,6 @@ export default function createNewGameState(
     move: string,
     state: IGameState,
 ): IGameState {
-    // @TODO test everything
     const boardArray = parseBoard(state.fenBoard);
 
     let isHalfMoveClockMove: boolean;
@@ -18,28 +17,29 @@ export default function createNewGameState(
 
     const parsedMove = parseMove(move);
     const [fromIndex, toIndex] = parsedMove.move;
-    const pieceToMove = parsedMove.promotionPiece ? parsedMove.promotionPiece : boardArray[fromIndex];
+    const pieceToMove = boardArray[fromIndex];
 
     if (!isPawn(pieceToMove) && !isCapture(pieceToMove, boardArray[toIndex])) {
         isHalfMoveClockMove = true;
     }
 
     boardArray[fromIndex] = ".";
-    boardArray[toIndex] = pieceToMove;
+    boardArray[toIndex] = parsedMove.promotionPiece ? parsedMove.promotionPiece : pieceToMove;
 
     if (parsedMove.castling) {
         const [rookFromIndex, rookToindex] = parsedMove.castling.rookMove;
         boardArray[rookFromIndex] = ".";
-        boardArray[rookToindex] = pieceToMove;
+        boardArray[rookToindex] = state.hasToPlay === 'w' ? 'R' : 'r';
         castlingLetter = parsedMove.castling.letter;
     }
 
+    // Updates the state before returning it
     state.fenBoard = updateFenBoard(composeBoardArrayToString(boardArray))
     if (state.hasToPlay === "b") state.fullMoveClock = incrementFullMoveClock(state.fullMoveClock);
     if (isHalfMoveClockMove) state.halfMoveClock = incrementHalfMoveClock(state.halfMoveClock);
     if (castlingLetter) state.availableCastlings = updateAvailableCastlings(state.availableCastlings, castlingLetter); 
     if (parsedMove.enPassantTarget) state.enPassantTarget = parsedMove.enPassantTarget;
-    state.hasToPlay = toggleHasToPlay(state.hasToPlay)
+    state.hasToPlay = toggleHasToPlay(state.hasToPlay);
 
     return state;
 }
--- tests/unit/createNewGameState/createNewGameState.spec.ts
+++ tests/unit/createNewGameState/createNewGameState.spec.ts
@@ -51,6 +51,174 @@ test("createNewGameState() | h2h4", () => {
     expect(received).toStrictEqual(expected);
 });
 
+test("createNewGameState() | k castling", () => {
+    const gameState = {
+        fenBoard: 'r3k2r/8/8/8/8/8/8/R3K2R',
+        hasToPlay: 'b',
+        availableCastlings: 'KQkq',
+        enPassantTarget: '-',
+        halfMoveClock: 0,
+        fullMoveClock: 1,
+    };
+    const received = createNewGameState('e8g8', gameState);
+    const expected = {
+        fenBoard: 'r4rk./8/8/8/8/8/8/R3K2R',
+        hasToPlay: 'w',
+        availableCastlings: 'KQ',
+        enPassantTarget: '-',
+        halfMoveClock: 1,
+        fullMoveClock: 2,
+    };
+    expect(received).toStrictEqual(expected);
+});
+
+test("createNewGameState() | q castling", () => {
+    const gameState = {
+        fenBoard: 'r3k2r/8/8/8/8/8/8/R3K2R',
+        hasToPlay: 'b',
+        availableCastlings: 'kq',
+        enPassantTarget: '-',
+        halfMoveClock: 0,
+        fullMoveClock: 1,
+    };
+    const received = createNewGameState('e8c8', gameState);
+    const expected = {
+        fenBoard: '2kr3r/8/8/8/8/8/8/R3K2R',
+        hasToPlay: 'w',
+        availableCastlings: '-',
+        enPassantTarget: '-',
+        halfMoveClock: 1,
+        fullMoveClock: 2,
+    };
+    expect(received).toStrictEqual(expected);
+});
+
+test("createNewGameState() | K castling", () => {
+    const gameState = {
+        fenBoard: 'r3k2r/8/8/8/8/8/8/R3K2R',
+        hasToPlay: 'w',
+        availableCastlings: 'KQkq',
+        enPassantTarget: '-',
+        halfMoveClock: 0,
+        fullMoveClock: 1,
+    };
+    const received = createNewGameState('e1g1', gameState);
+    const expected = {
+        fenBoard: 'r3k2r/8/8/8/8/8/8/R4RK.',
+        hasToPlay: 'b',
+        availableCastlings: 'kq',
+        enPassantTarget: '-',
+        halfMoveClock: 1,
+        fullMoveClock: 1,
+    };
+    expect(received).toStrictEqual(expected);
+});
+
+test("createNewGameState() | Q castling", () => {
+    const gameState = {
+        fenBoard: 'r3k2r/8/8/8/8/8/8/R3K2R',
+        hasToPlay: 'w',
+        availableCastlings: 'KQ',
+        enPassantTarget: '-',
+        halfMoveClock: 0,
+        fullMoveClock: 1,
+    };
+    const received = createNewGameState('e1c1', gameState);
+    const expected = {
+        fenBoard: 'r3k2r/8/8/8/8/8/8/2KR3R',
+        hasToPlay: 'b',
+        availableCastlings: '-',
+        enPassantTarget: '-',
+        halfMoveClock: 1,
+        fullMoveClock: 1,
+    };
+    expect(received).toStrictEqual(expected);
+});
+
+test("createNewGameState() | Q promotion move", () => {
+    const gameState = {
+        fenBoard: '8/P7/8/8/8/8/8/8',
+        hasToPlay: 'w',
+        availableCastlings: 'KQkq',
+        enPassantTarget: '-',
+        halfMoveClock: 0,
+        fullMoveClock: 1,
+    };
+    const received = createNewGameState('a7a8Q', gameState);
+    const expected = {
+        fenBoard: 'Q7/8/8/8/8/8/8/8',
+        hasToPlay: 'b',
+        availableCastlings: 'KQkq',
+        enPassantTarget: '-',
+        halfMoveClock: 0,
+        fullMoveClock: 1,
+    };
+    expect(received).toStrictEqual(expected);
+});
+
+test("createNewGameState() | N promotion move", () => {
+    const gameState = {
+        fenBoard: '8/7P/8/8/8/8/8/8',
+        hasToPlay: 'w',
+        availableCastlings: 'KQkq',
+        enPassantTarget: '-',
+        halfMoveClock: 0,
+        fullMoveClock: 1,
+    };
+    const received = createNewGameState('h7h8N', gameState);
+    const expected = {
+        fenBoard: '7N/8/8/8/8/8/8/8',
+        hasToPlay: 'b',
+        availableCastlings: 'KQkq',
+        enPassantTarget: '-',
+        halfMoveClock: 0,
+        fullMoveClock: 1,
+    };
+    expect(received).toStrictEqual(expected);
+});
+
+test("createNewGameState() | b promotion move", () => {
+    const gameState = {
+        fenBoard: '8/8/8/8/8/8/p7/8',
+        hasToPlay: 'b',
+        availableCastlings: 'KQkq',
+        enPassantTarget: '-',
+        halfMoveClock: 0,
+        fullMoveClock: 1,
+    };
+    const received = createNewGameState('a2a1b', gameState);
+    const expected = {
+        fenBoard: '8/8/8/8/8/8/8/b7',
+        hasToPlay: 'w',
+        availableCastlings: 'KQkq',
+        enPassantTarget: '-',
+        halfMoveClock: 0,
+        fullMoveClock: 2,
+    };
+    expect(received).toStrictEqual(expected);
+});
+
+test("createNewGameState() | r promotion move", () => {
+    const gameState = {
+        fenBoard: '8/8/8/8/8/8/7p/8',
+        hasToPlay: 'b',
+        availableCastlings: 'KQkq',
+        enPassantTarget: '-',
+        halfMoveClock: 0,
+        fullMoveClock: 1,
+    };
+    const received = createNewGameState('h2h1r', gameState);
+    const expected = {
+        fenBoard: '8/8/8/8/8/8/8/7r',
+        hasToPlay: 'w',
+        availableCastlings: 'KQkq',
+        enPassantTarget: '-',
+        halfMoveClock: 0,
+        fullMoveClock: 2,
+    };
+    expect(received).toStrictEqual(expected);
+});
+
 test("isPawn() | pawn", () => {
     const received1 = isPawn('p');
     const received2 = isPawn('P');
