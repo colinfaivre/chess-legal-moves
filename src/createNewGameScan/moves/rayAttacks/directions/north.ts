714dbf7904e9245ecb2a494cf8687f8258f47694
Colin Faivre <colin.faivre@gmail.com>
2022-05-27 13:26:11 +0200

    refactor: refactor north attacks

--- src/createNewGameScan/moves/rayAttacks/directions/north.ts
+++ src/createNewGameScan/moves/rayAttacks/directions/north.ts
@@ -1,5 +1,6 @@
 import { IRayAttack } from "../../../../types";
 import BitBoard from "../../../BitBoard/BitBoard";
+import { RAYS } from "../rays";
 
 /**
  * Populates an array of 64 IRayAttacks objects with north attacks
@@ -19,12 +20,11 @@ export function generateNorthAttacks(attacksList: IRayAttack[]): IRayAttack[] {
      *   ABCDEFGH
      *************/
     // A2_A8 north attack mask is used as a placeholder to be slid on each square
-    const A2_A8 = BitBoard.fromHex('0101010101010100');
-    let northAttackMask: BitBoard = A2_A8;
+    let northAttackMask: BitBoard = RAYS.A2_A8;
 
     // Loop through each square of the board a1, a2, ... h7, h8
     // populate each attacksList square north property
-    // slide the mask to the right of the next iteration
+    // slide the mask to the right for the next iteration
     for (let square = 0; square < 64; square++) {
         attacksList[square].no = northAttackMask;
         northAttackMask = northAttackMask.shiftLeft(1);
