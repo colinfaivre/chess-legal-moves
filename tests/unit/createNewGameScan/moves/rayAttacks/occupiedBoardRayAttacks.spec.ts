ebbf80eae6b597eeb8797330f81d03e161bf455c
Colin Faivre <colin.faivre@gmail.com>
2022-06-02 17:07:11 +0200

    test: :white_check_mark: add unit tests for getPositiveRayAttacks() and getNegativeRayAttacks()

--- tests/unit/createNewGameScan/moves/rayAttacks/occupiedBoardRayAttacks.spec.ts
+++ tests/unit/createNewGameScan/moves/rayAttacks/occupiedBoardRayAttacks.spec.ts
@@ -2,7 +2,9 @@ import BitBoard from "../../../../../src/createNewGameScan/BitBoard/BitBoard";
 import Board from "../../../../../src/createNewGameScan/Board/Board";
 import {
     getPositiveRayAttacks,
+    getNegativeRayAttacks,
 } from "../../../../../src/createNewGameScan/moves/rayAttacks/occupiedBoardRayAttacks";
+import { RAYS } from "../../../../../src/createNewGameScan/moves/rayAttacks/rays"
 
 test("getPositiveRayAttacks() | starting position - north - a1 rook", () => {
     const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
@@ -16,6 +18,22 @@ test("getPositiveRayAttacks() | starting position - east - a1 rook", () => {
     const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
     const received = getPositiveRayAttacks(startingBitBoard, 'ea', 0);
     const expected = BitBoard.fromPos(1);
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getNegativeRayAttacks() | starting position - south - a1 rook", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getNegativeRayAttacks(startingBitBoard, 'so', 0);
+    const expected = RAYS.NULL;
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getNegativeRayAttacks() | starting position - west - a1 rook", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getNegativeRayAttacks(startingBitBoard, 'we', 0);
+    const expected = RAYS.NULL;
 
     expect(received).toStrictEqual(expected);
 });
@@ -48,6 +66,110 @@ test("getPositiveRayAttacks() | starting position - north-east - d1 queen", () =
     const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
     const received = getPositiveRayAttacks(startingBitBoard, 'noEa', 3);
     const expected = BitBoard.fromPositions([12]);
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getPositiveRayAttacks() | starting position - east - d1 queen", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getPositiveRayAttacks(startingBitBoard, 'ea', 3);
+    const expected = BitBoard.fromPositions([4]);
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getPositiveRayAttacks() | starting position - north-west - d1 queen", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getPositiveRayAttacks(startingBitBoard, 'noWe', 3);
+    const expected = BitBoard.fromPositions([10]);
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getPositiveRayAttacks() | starting position - south - d1 queen", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getNegativeRayAttacks(startingBitBoard, 'so', 3);
+    const expected = RAYS.NULL;
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getPositiveRayAttacks() | starting position - west - d1 queen", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getNegativeRayAttacks(startingBitBoard, 'we', 3);
+    const expected = BitBoard.fromPositions([2]);
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getPositiveRayAttacks() | starting position - south-west - d1 queen", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getNegativeRayAttacks(startingBitBoard, 'soWe', 3);
+    const expected = RAYS.NULL;
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getPositiveRayAttacks() | starting position - south-east - d1 queen", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getNegativeRayAttacks(startingBitBoard, 'soEa', 3);
+    const expected = RAYS.NULL;
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getPositiveRayAttacks() | starting position - north-east - d1 queen", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getPositiveRayAttacks(startingBitBoard, 'noEa', 27);
+    const expected = BitBoard.fromPositions([36, 45, 54]);
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getPositiveRayAttacks() | queen party - east - d4 queen", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getPositiveRayAttacks(startingBitBoard, 'ea', 27);
+    const expected = BitBoard.fromPositions([28, 29, 30, 31]);
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getPositiveRayAttacks() | queen party - north-west - d4 queen", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getPositiveRayAttacks(startingBitBoard, 'noWe', 27);
+    const expected = BitBoard.fromPositions([34, 41, 48]);
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getPositiveRayAttacks() | queen party - south - d4 queen", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getNegativeRayAttacks(startingBitBoard, 'so', 27);
+    const expected = BitBoard.fromPositions([19, 11]);
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getPositiveRayAttacks() | queen party - west - d4 queen", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getNegativeRayAttacks(startingBitBoard, 'we', 27);
+    const expected = BitBoard.fromPositions([24, 25, 26]);
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getPositiveRayAttacks() | queen party - south-west - d4 queen", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getNegativeRayAttacks(startingBitBoard, 'soWe', 27);
+    const expected = BitBoard.fromPositions([18, 9]);
+
+    expect(received).toStrictEqual(expected);
+});
+
+test("getPositiveRayAttacks() | queen party - south-east - d4 queen", () => {
+    const startingBitBoard: BitBoard = new Board('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR').allPieces
+    const received = getNegativeRayAttacks(startingBitBoard, 'soEa', 27);
+    const expected = BitBoard.fromPositions([20, 13]);
 
     expect(received).toStrictEqual(expected);
 });
