cda937b05b8d458578303687bc6738232e175e9e
Colin Faivre <colin.faivre@gmail.com>
2022-06-02 16:01:27 +0200

    docs: :memo: add documentation for generateSouthWestAttacks()

--- src/createNewGameScan/moves/rayAttacks/directions/southWest.ts
+++ src/createNewGameScan/moves/rayAttacks/directions/southWest.ts
@@ -1,10 +1,13 @@
 import { IRayAttack } from "../../../../types";
 import BitBoard from "../../../BitBoard/BitBoard";
+import { RAYS } from "../rays";
 
+/**
+ * Populates an array of 64 IRayAttacks objects with south-west attacks
+ * @param attacksList an IRayAttack array of directional attacks
+ * @returns attacksList populated with south-west attacks
+ */
 export function generateSouthWestAttacks(attacksList: IRayAttack[]): IRayAttack[] {
-    // @TODO document
-    // @TODO add tests
-    
     /**************
      * 8 .......x *
      * 7 ......1. *
@@ -16,25 +19,42 @@ export function generateSouthWestAttacks(attacksList: IRayAttack[]): IRayAttack[
      * 1 1....... * -- G7_A1 
      *   ABCDEFGH
      *************/
+    // G7_A1 south-west attack mask is used as a placeholder to be slid on each square
+    let southWestAttackMask: BitBoard = RAYS.G7_A1;
 
-    // @TODO try again it does not work on 7
-    const G7_A1 = BitBoard.fromHex('0040201008040201');
-
-    let southWestAttackMask: BitBoard = G7_A1;
-    for (let file = 0; file < 8; file++, southWestAttackMask = getNextSouthWestMask(southWestAttackMask)) {
+    // Loop through each file of the board: A, B, ... G, H
+    // at the end of each loop, slide the mask up and remove the highest bit for the next iteration
+    for (let file = 0; file < 8; file++) {
+        // Set temporary south-west mask to be used by the following for loop
         let southWest = southWestAttackMask;
-        for (let r8 = 63; r8 >= 8; r8 -= 8) {
-            attacksList[r8 - file].soWe = southWest;
-            southWest = southWest.shiftRight(8);
-        } 
+        // Loop through each square (from top to bottom) on the current file
+        // at the end of each loop, slide down the mask for the next iteration
+        for (let square = 63; square >= 0; square -= 8) {
+            // populate each attacksList square south-west property
+            attacksList[square - file].soWe = southWest;
+            southWest = slideDown(southWest);
+        }
+        southWestAttackMask = slideUp(southWestAttackMask);
+        southWestAttackMask = removeHighestBit(southWestAttackMask);
     }
 
     return attacksList;
 }
 
-export function getNextSouthWestMask(attacks: BitBoard): BitBoard {
+export function slideUp(attacks: BitBoard): BitBoard {
     attacks = attacks.shiftLeft(8);
-    attacks.clearBit(attacks.bitScanReverse());
+
+    return attacks;
+}
+
+export function slideDown(attacks: BitBoard): BitBoard {
+    attacks = attacks.shiftRight(8);
+
+    return attacks;
+}
+
+export function removeHighestBit(attacks: BitBoard): BitBoard {
+    attacks = attacks.clearBit(attacks.bitScanReverse());
 
     return attacks;
 }
